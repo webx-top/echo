132aa517fc30413bf38648c037979ac45bf8a3f7
swh <swhbox@foxmail.com>
2016-10-25 14:32:57 +0800

    update

--- echo.go
+++ echo.go
@@ -680,6 +680,12 @@ func WrapMiddleware(m interface{}) Middleware {
 			return HandlerFunc(h(next.Handle))
 		})
 	}
+	if v, ok := m.(http.Handler); ok {
+		return WrapMiddlewareFromStdHandler(v)
+	}
+	if v, ok := m.(func(http.ResponseWriter, *http.Request)); ok {
+		return WrapMiddlewareFromStdHandleFunc(v)
+	}
 	panic(`unknown middleware`)
 }
 
@@ -689,6 +695,30 @@ func WrapMiddlewareFromHandler(h HandlerFunc) Middleware {
 		return HandlerFunc(func(c Context) error {
 			if err := h.Handle(c); err != nil {
 				return err
+			}
+			return next.Handle(c)
+		})
+	})
+}
+
+func WrapMiddlewareFromStdHandler(h http.Handler) Middleware {
+	return MiddlewareFunc(func(next Handler) Handler {
+		return HandlerFunc(func(c Context) error {
+			h.ServeHTTP(c.Response().StdResponseWriter(), c.Request().StdRequest())
+			if c.Response().Committed() {
+				return nil
+			}
+			return next.Handle(c)
+		})
+	})
+}
+
+func WrapMiddlewareFromStdHandleFunc(h func(http.ResponseWriter, *http.Request)) Middleware {
+	return MiddlewareFunc(func(next Handler) Handler {
+		return HandlerFunc(func(c Context) error {
+			h(c.Response().StdResponseWriter(), c.Request().StdRequest())
+			if c.Response().Committed() {
+				return nil
 			}
 			return next.Handle(c)
 		})
