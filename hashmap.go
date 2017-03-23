cc3d0a45c425655bb5351967683ec8741a170a9c
swh <swhbox@foxmail.com>
2017-03-23 16:10:54 +0800

    update

--- hashmap.go
+++ hashmap.go
@@ -43,9 +43,11 @@ type H map[string]interface{}
 
 // MarshalXML allows type H to be used with xml.Marshal
 func (h H) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	start.Name = xml.Name{
-		Space: ``,
-		Local: `Map`,
+	if start.Name.Local == `H` {
+		start.Name = xml.Name{
+			Space: ``,
+			Local: `Map`,
+		}
 	}
 	if err := e.EncodeToken(start); err != nil {
 		return err
