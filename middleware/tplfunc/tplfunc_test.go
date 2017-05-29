38f4cc94e4adace461ad2f2f441c9482fcbbf1aa
Wenhui Shen <swhbox@foxmail.com>
2017-05-29 22:36:31 +0800

    added func: NumberFormat

---
+++ middleware/tplfunc/tplfunc_test.go
@@ -0,0 +1,15 @@
+package tplfunc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNumberFormat(t *testing.T) {
+	assert.Equal(t, "12.99", NumberFormat(12.987, 2))
+	assert.Equal(t, "123.99", NumberFormat(123.987, 2))
+	assert.Equal(t, "1,234.99", NumberFormat(1234.987, 2))
+	assert.Equal(t, "1,234,567.99", NumberFormat(1234567.987, 2))
+	assert.Equal(t, "234,567.99", NumberFormat(234567.987, 2))
+}
