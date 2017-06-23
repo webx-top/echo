e72e8cbcbd4bf64bd3f7244afebf520b51a161b0
Wenhui Shen <swhbox@foxmail.com>
2017-06-23 13:37:33 +0800

    binder:  improved

---
+++ binder_test.go
@@ -0,0 +1,54 @@
+package echo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestForm struct {
+	Files []string
+	Data  *TestData
+}
+
+type TestData struct {
+	File *TestFile
+}
+
+type TestFile struct {
+	Users []int64
+}
+
+func TestMapToStruct(t *testing.T) {
+	e := New()
+	m := &TestForm{}
+	NamedStructMap(e, m, map[string][]string{
+		`files[]`:             []string{`a.txt`, `b.txt`, `c.txt`},
+		`data[file][users][]`: []string{`1`, `2`, `3`},
+	}, ``)
+	assert.Equal(t, &TestForm{
+		Files: []string{`a.txt`, `b.txt`, `c.txt`},
+		Data: &TestData{
+			File: &TestFile{
+				Users: []int64{1, 2, 3},
+			},
+		},
+	}, m)
+}
+
+func TestMapToStruct2(t *testing.T) {
+	e := New()
+	m := &TestForm{}
+	NamedStructMap(e, m, map[string][]string{
+		`files`:             []string{`a.txt`, `b.txt`, `c.txt`},
+		`data[file][users]`: []string{`1`, `2`, `3`},
+	}, ``)
+	assert.Equal(t, &TestForm{
+		Files: []string{`a.txt`, `b.txt`, `c.txt`},
+		Data: &TestData{
+			File: &TestFile{
+				Users: []int64{1, 2, 3},
+			},
+		},
+	}, m)
+}
