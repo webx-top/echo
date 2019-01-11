0e78272fc7b0721ce5ad10f23eadeaa69ff46893
Hank Shen <swhbox@foxmail.com>
2019-01-11 10:50:19 +0800

    update

--- host/error.go
+++
@@ -1,9 +0,0 @@
-package host
-
-import "errors"
-
-var (
-	ErrHasBeenDisabled     = errors.New(`This module has been disabled`)
-	ErrHasNotBeenInstalled = errors.New(`This module has not been installed`)
-	ErrHasExpired          = errors.New(`This module has expired`)
-)
