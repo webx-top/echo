eeefec572b294687414839d28af80a6c8b18c195
Hank Shen <swhbox@foxmail.com>
2019-09-06 00:53:03 +0800

    update

--- param/conversion.go
+++ param/conversion.go
@@ -24,6 +24,43 @@ const (
 	TimeShort      = `15:04`
 )
 
+func AsType(typ string, val interface{}) interface{} {
+	switch typ {
+	case `string`:
+		return AsString(val)
+	case `bytes`:
+		return AsBytes(val)
+	case `bool`:
+		return AsBool(val)
+	case `float64`:
+		return AsFloat64(val)
+	case `float32`:
+		return AsFloat32(val)
+	case `int8`:
+		return AsInt8(val)
+	case `int16`:
+		return AsInt16(val)
+	case `int`:
+		return AsInt(val)
+	case `int32`:
+		return AsInt32(val)
+	case `int64`:
+		return AsInt64(val)
+	case `uint8`:
+		return AsUint8(val)
+	case `uint16`:
+		return AsUint16(val)
+	case `uint`:
+		return AsUint(val)
+	case `uint32`:
+		return AsUint32(val)
+	case `uint64`:
+		return AsUint64(val)
+	default:
+		return val
+	}
+}
+
 func AsString(val interface{}) string {
 	switch v := val.(type) {
 	case string:
