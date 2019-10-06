e9c077523b17dd4fe7f6d1bdc4d321e390ba63b5
Hank Shen <swhbox@foxmail.com>
2019-10-06 11:39:56 +0800

    update

---
+++ middleware/opentracing/opentracing.go
@@ -0,0 +1,95 @@
+package opentracing
+
+import (
+	"net/http"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+
+	"github.com/webx-top/echo"
+)
+
+const defaultComponentName = "webx-top/echo"
+
+type (
+	// TraceConfig defines the config for Trace middleware.
+	TraceConfig struct {
+		// Skipper defines a function to skip middleware.
+		Skipper echo.Skipper
+
+		// OpenTracing Tracer instance which should be got before
+		tracer opentracing.Tracer
+
+		// componentName used for describing the tracing component name
+		componentName string
+	}
+)
+
+var (
+	// DefaultTraceConfig is the default Trace middleware config.
+	DefaultTraceConfig = TraceConfig{
+		Skipper:       echo.DefaultSkipper,
+		componentName: defaultComponentName,
+	}
+)
+
+// Trace returns a Trace middleware.
+//
+// Trace middleware traces http requests and reporting errors.
+func Trace(tracer opentracing.Tracer) echo.MiddlewareFunc {
+	c := DefaultTraceConfig
+	c.tracer = tracer
+	c.componentName = defaultComponentName
+	return TraceWithConfig(c)
+}
+
+// TraceWithConfig returns a Trace middleware with config.
+// See: `Trace()`.
+func TraceWithConfig(config TraceConfig) echo.MiddlewareFunc {
+	if config.tracer == nil {
+		panic("echo: trace middleware requires opentracing tracer")
+	}
+	if config.Skipper == nil {
+		config.Skipper = echo.DefaultSkipper
+	}
+	if len(config.componentName) == 0 {
+		config.componentName = defaultComponentName
+	}
+
+	return func(h echo.Handler) echo.Handler {
+		return echo.HandlerFunc(func(c echo.Context) error {
+			if config.Skipper(c) {
+				return h.Handle(c)
+			}
+
+			req := c.Request()
+			opname := "HTTP " + req.Method()
+			var sp opentracing.Span
+			tr := config.tracer
+			if ctx, err := tr.Extract(opentracing.HTTPHeaders,
+				opentracing.HTTPHeadersCarrier(req.Header().Std())); err != nil {
+				sp = tr.StartSpan(opname)
+			} else {
+				sp = tr.StartSpan(opname, ext.RPCServerOption(ctx))
+			}
+
+			ext.HTTPMethod.Set(sp, req.Method())
+			ext.HTTPUrl.Set(sp, req.URL().String())
+			ext.Component.Set(sp, config.componentName)
+
+			c.SetStdContext(opentracing.ContextWithSpan(c.StdContext(), sp))
+
+			defer func() {
+				status := c.Response().Status()
+				committed := c.Response().Committed()
+				ext.HTTPStatusCode.Set(sp, uint16(status))
+				if status >= http.StatusInternalServerError || !committed {
+					ext.Error.Set(sp, true)
+				}
+				sp.Finish()
+			}()
+
+			return h.Handle(c)
+		})
+	}
+}
---
+++ middleware/opentracing/opentracing_test.go
@@ -0,0 +1,206 @@
+package opentracing
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/log"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/webx-top/echo"
+	te "github.com/webx-top/echo/testing"
+)
+
+// Mock opentracing.Span
+type mockSpan struct {
+	tracer   opentracing.Tracer
+	tags     map[string]interface{}
+	opName   string
+	finished bool
+}
+
+func createSpan(tracer opentracing.Tracer) *mockSpan {
+	return &mockSpan{
+		tracer: tracer,
+		tags:   make(map[string]interface{}),
+	}
+}
+
+func (sp *mockSpan) isFinished() bool {
+	return sp.finished
+}
+
+func (sp *mockSpan) getOpName() string {
+	return sp.opName
+}
+
+func (sp *mockSpan) getTag(key string) interface{} {
+	return sp.tags[key]
+}
+
+func (sp *mockSpan) Finish() {
+	sp.finished = true
+}
+func (sp *mockSpan) FinishWithOptions(opts opentracing.FinishOptions) {
+}
+func (sp *mockSpan) Context() opentracing.SpanContext {
+	return nil
+}
+func (sp *mockSpan) SetOperationName(operationName string) opentracing.Span {
+	sp.opName = operationName
+	return sp
+}
+func (sp *mockSpan) SetTag(key string, value interface{}) opentracing.Span {
+	sp.tags[key] = value
+	return sp
+}
+func (sp *mockSpan) LogFields(fields ...log.Field) {
+}
+func (sp *mockSpan) LogKV(alternatingKeyValues ...interface{}) {
+}
+func (sp *mockSpan) SetBaggageItem(restrictedKey, value string) opentracing.Span {
+	return sp
+}
+func (sp *mockSpan) BaggageItem(restrictedKey string) string {
+	return ""
+}
+func (sp *mockSpan) Tracer() opentracing.Tracer {
+	return sp.tracer
+}
+func (sp *mockSpan) LogEvent(event string) {
+}
+func (sp *mockSpan) LogEventWithPayload(event string, payload interface{}) {
+}
+func (sp *mockSpan) Log(data opentracing.LogData) {
+}
+
+// Mock opentracing.Tracer
+type mockTracer struct {
+	span                   *mockSpan
+	hasStartSpanWithOption bool
+}
+
+func (tr *mockTracer) currentSpan() *mockSpan {
+	return tr.span
+}
+
+func (tr *mockTracer) StartSpan(operationName string, opts ...opentracing.StartSpanOption) opentracing.Span {
+	tr.hasStartSpanWithOption = (len(opts) > 0)
+	if tr.span != nil {
+		return tr.span
+	}
+	return createSpan(tr)
+}
+
+func (tr *mockTracer) Inject(sm opentracing.SpanContext, format interface{}, carrier interface{}) error {
+	return nil
+}
+
+func (tr *mockTracer) Extract(format interface{}, carrier interface{}) (opentracing.SpanContext, error) {
+	if tr.span != nil {
+		return nil, nil
+	}
+	return nil, errors.New("no span")
+}
+
+func createMockTracer() *mockTracer {
+	tracer := mockTracer{}
+	span := createSpan(&tracer)
+	tracer.span = span
+	return &tracer
+}
+
+func TestTraceWithDefaultConfig(t *testing.T) {
+	tracer := createMockTracer()
+
+	e := echo.New()
+	e.Use(Trace(tracer))
+	e.Get(`/`, func(c echo.Context) error {
+		return c.String(`ok`)
+	})
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(te.WrapRequest(req), te.WrapResponse(req, rec))
+
+	assert.Equal(t, "GET", tracer.currentSpan().getTag("http.method"))
+	assert.Equal(t, "/", tracer.currentSpan().getTag("http.url"))
+	assert.Equal(t, defaultComponentName, tracer.currentSpan().getTag("component"))
+	assert.Equal(t, uint16(200), tracer.currentSpan().getTag("http.status_code"))
+	assert.Equal(t, nil, tracer.currentSpan().getTag("error"))
+}
+
+func TestTraceWithConfig(t *testing.T) {
+	tracer := createMockTracer()
+
+	e := echo.New()
+	e.Use(TraceWithConfig(TraceConfig{
+		tracer:        tracer,
+		componentName: "EchoTracer",
+	}))
+	e.Get(`/trace`, func(c echo.Context) error {
+		return c.String(`ok`)
+	})
+	req := httptest.NewRequest(http.MethodGet, "/trace", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(te.WrapRequest(req), te.WrapResponse(req, rec))
+
+	assert.Equal(t, true, tracer.currentSpan().isFinished())
+	assert.Equal(t, "/trace", tracer.currentSpan().getTag("http.url"))
+	assert.Equal(t, "EchoTracer", tracer.currentSpan().getTag("component"))
+	assert.Equal(t, true, tracer.hasStartSpanWithOption)
+
+}
+
+func TestTraceWithConfigOfNoneComponentName(t *testing.T) {
+	tracer := createMockTracer()
+
+	e := echo.New()
+	e.Use(TraceWithConfig(TraceConfig{
+		tracer: tracer,
+	}))
+	e.Get(`/`, func(c echo.Context) error {
+		return c.String(`ok`)
+	})
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(te.WrapRequest(req), te.WrapResponse(req, rec))
+
+	assert.Equal(t, true, tracer.currentSpan().isFinished())
+	assert.Equal(t, defaultComponentName, tracer.currentSpan().getTag("component"))
+}
+
+func TestTraceWithConfigOfSkip(t *testing.T) {
+	tracer := createMockTracer()
+	e := echo.New()
+	e.Use(TraceWithConfig(TraceConfig{
+		Skipper: func(echo.Context) bool {
+			return true
+		},
+		tracer: tracer,
+	}))
+	e.Get(`/`, func(c echo.Context) error {
+		return c.String(`ok`)
+	})
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(te.WrapRequest(req), te.WrapResponse(req, rec))
+
+	assert.Equal(t, false, tracer.currentSpan().isFinished())
+}
+
+func TestTraceOfNoCurrentSpan(t *testing.T) {
+	tracer := &mockTracer{}
+	e := echo.New()
+	e.Use(Trace(tracer))
+	e.Get(`/`, func(c echo.Context) error {
+		return c.String(`ok`)
+	})
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(te.WrapRequest(req), te.WrapResponse(req, rec))
+
+	assert.Equal(t, false, tracer.hasStartSpanWithOption)
+}
