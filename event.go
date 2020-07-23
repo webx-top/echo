20986819933d184d99be883f8856899b0140167e
Hank Shen <swhbox@foxmail.com>
2020-07-23 14:58:06 +0800

    update

---
+++ event.go
@@ -0,0 +1,39 @@
+package echo
+
+import (
+	"github.com/admpub/events"
+	"github.com/admpub/events/emitter"
+)
+
+func On(name string, cb func(H) error, onErrorDie ...bool) {
+	var onErrorStop bool
+	if len(onErrorDie) > 0 {
+		onErrorStop = onErrorDie[0]
+	}
+	emitter.DefaultCondEmitter.On(name, events.Callback(func(e events.Event) error {
+		if onErrorStop {
+			err := cb(e.Context)
+			if err != nil {
+				e.Abort()
+			}
+			return err
+		}
+		return cb(e.Context)
+	}))
+}
+
+func Off(name string) {
+	emitter.DefaultCondEmitter.Off(name)
+}
+
+func Fire(e interface{}, mode int, context ...H) error {
+	return emitter.DefaultCondEmitter.Fire(e, mode, context...)
+}
+
+func Events() map[string]events.Dispatcher {
+	return emitter.DefaultCondEmitter.Events()
+}
+
+func HasEvent(name string) bool {
+	return emitter.DefaultCondEmitter.HasEvent(name)
+}
