a717c59da5a2adb1b8933c675ac8b1f6a96b594d
Hank Shen <swhbox@foxmail.com>
2020-08-14 13:36:26 +0800

    update

---
+++ param/string_slice_test.go
@@ -0,0 +1,19 @@
+package param
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSlice(t *testing.T) {
+	v := StringSlice([]string{"a", "a", "b", "c", "c", "e", "e"})
+	r := v.Unique().Join(`,`)
+	if r != `a,b,c,e` {
+		panic(fmt.Sprintf("%v != b,b,c,e", r))
+	}
+	v = StringSlice([]string{"a", "a"})
+	r = v.Unique().Join(`,`)
+	if r != `a` {
+		panic(fmt.Sprintf("%v != a", r))
+	}
+}
