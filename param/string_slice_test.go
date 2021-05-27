5b4e640d14c18a70555d482e16c9001cc32b6960
Hank Shen <swhbox@foxmail.com>
2021-05-27 11:23:19 +0800

    update

--- param/string_slice_test.go
+++ param/string_slice_test.go
@@ -9,7 +9,7 @@ func TestSlice(t *testing.T) {
 	v := StringSlice([]string{"a", "a", "b", "c", "c", "e", "e"})
 	r := v.Unique().Join(`,`)
 	if r != `a,b,c,e` {
-		panic(fmt.Sprintf("%v != b,b,c,e", r))
+		panic(fmt.Sprintf("%v != a,b,c,e", r))
 	}
 	v = StringSlice([]string{"a", "a"})
 	r = v.Unique().Join(`,`)
