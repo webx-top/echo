8141b72e180c192c00e8fb43c9638cfd9841a411
Hank Shen <swhbox@foxmail.com>
2021-09-10 21:42:55 +0800

    update

--- echo_test.go
+++ echo_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/admpub/log"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/webx-top/echo"
 	. "github.com/webx-top/echo"
 	mw "github.com/webx-top/echo/middleware"
 	test "github.com/webx-top/echo/testing"
@@ -205,21 +204,6 @@ func TestEchoHandler(t *testing.T) {
 	e.Get("/file/*", func(c Context) error {
 		return c.String(c.P(0))
 	})
-	g := e.Host(".admpub.com")
-	g.Get("/host", func(c Context) error {
-		if c.Queryx(`route`).Bool() {
-			return c.JSON(c.Route())
-		}
-		return c.String(c.Host())
-	}).SetName(`host`)
-	g = e.Host("<uid:[0-9]+>.<name>.com").SetAlias(`user`)
-	g.Get("/host2", func(c Context) error {
-		if c.Queryx(`route`).Bool() {
-			return c.JSON(c.Route())
-		}
-		return c.String(c.HostParam(`uid`) + `.` + c.HostParam(`name`) + `.com`)
-	}).SetName(`host2`)
-
 	e.RebuildRouter()
 
 	assert.Equal(t, `/view/8`, e.URI(`view`, 8))
@@ -236,29 +220,6 @@ func TestEchoHandler(t *testing.T) {
 	c, b = request(GET, "/file/path/to/file.js", e)
 	assert.Equal(t, http.StatusOK, c)
 	assert.Equal(t, "path/to/file.js", b)
-	c, b = request(GET, "/host", e, func(req *http.Request) {
-		req.Host = "test.admpub.com"
-	})
-	assert.Equal(t, http.StatusOK, c)
-	assert.Equal(t, "test.admpub.com", b)
-	c, b = request(GET, "/host", e, func(req *http.Request) {
-		req.Host = "test-b.admpub.com"
-	})
-	assert.Equal(t, http.StatusOK, c)
-	assert.Equal(t, "test-b.admpub.com", b)
-	c, b = request(GET, "/host?route=1", e, func(req *http.Request) {
-		req.Host = "test-b.admpub.com"
-	})
-	assert.Equal(t, http.StatusOK, c)
-	assert.Equal(t, `{"Host":".admpub.com","Method":"GET","Path":"/host","Name":"host","Format":"/host","Params":[],"Prefix":"","Meta":{}}`, b)
-
-	c, b = request(GET, "/host2", e, func(req *http.Request) {
-		req.Host = "123.coscms.com"
-	})
-	assert.Equal(t, http.StatusOK, c)
-	assert.Equal(t, "123.coscms.com", b)
-	assert.Equal(t, "10000.admpub.com/host2", e.TypeHost(`user`, 10000, `admpub`).URI(`host2`))
-	assert.Equal(t, "10001.admpub.com/host2", e.TypeHost(`user`, echo.H{`uid`: 10001, `name`: `admpub`}).URI(`host2`))
 }
 
 func TestEchoRouter(t *testing.T) {
--- host_test.go
+++ host_test.go
@@ -1,14 +1,18 @@
 package echo_test
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
 	"testing"
 
-	"github.com/webx-top/echo"
+	"github.com/stretchr/testify/assert"
+	. "github.com/webx-top/echo"
 	"github.com/webx-top/echo/testing/test"
 )
 
 func TestHostParse(t *testing.T) {
-	h := echo.NewHost(`<a:[0-9]+>.<b>.cc`)
+	h := NewHost(`<a:[0-9]+>.<b>.cc`)
 	h.Parse()
 	test.Eq(t, `%v.%v.cc`, h.Format())
 	test.Eq(t, `^([0-9]+)\.([^.]+)\.cc$`, h.RegExp().String())
@@ -18,4 +22,80 @@ func TestHostParse(t *testing.T) {
 		`1`,
 		`baa`,
 	}, r)
+}
+
+func TestHostGroupHandler(t *testing.T) {
+	e := New()
+	e.OnHostFound(func(c Context) (bool, error) {
+		if c.HostParam(`name`) != `coscms` {
+			return true, nil
+		}
+		fmt.Println(`host param (uid):`, c.HostParam(`uid`))
+		switch c.HostParam(`uid`) {
+		case `123`:
+			return true, nil
+		case `0`:
+			return false, errors.New(`err`)
+		default:
+			return false, nil
+		}
+	})
+
+	// suffix
+	g := e.Host(".admpub.com")
+	g.Get("/host", func(c Context) error {
+		if c.Queryx(`route`).Bool() {
+			return c.JSON(c.Route())
+		}
+		return c.String(c.Host())
+	}).SetName(`host`)
+
+	// rule
+	g = e.Host("<uid:[0-9]+>.<name>.com").SetAlias(`user`)
+	g.Get("/host2", func(c Context) error {
+		if c.Queryx(`route`).Bool() {
+			return c.JSON(c.Route())
+		}
+		return c.String(c.HostParam(`uid`) + `.` + c.HostParam(`name`) + `.com`)
+	}).SetName(`host2`)
+
+	e.RebuildRouter()
+
+	c, b := request(GET, "/host", e, func(req *http.Request) {
+		req.Host = "test.admpub.com"
+	})
+	assert.Equal(t, http.StatusOK, c)
+	assert.Equal(t, "test.admpub.com", b)
+
+	c, b = request(GET, "/host", e, func(req *http.Request) {
+		req.Host = "test-b.admpub.com"
+	})
+	assert.Equal(t, http.StatusOK, c)
+	assert.Equal(t, "test-b.admpub.com", b)
+
+	c, b = request(GET, "/host?route=1", e, func(req *http.Request) {
+		req.Host = "test-b.admpub.com"
+	})
+	assert.Equal(t, http.StatusOK, c)
+	assert.Equal(t, `{"Host":".admpub.com","Method":"GET","Path":"/host","Name":"host","Format":"/host","Params":[],"Prefix":"","Meta":{}}`, b)
+
+	c, b = request(GET, "/host2", e, func(req *http.Request) {
+		req.Host = "123.coscms.com"
+	})
+	assert.Equal(t, http.StatusOK, c)
+	assert.Equal(t, "123.coscms.com", b)
+	assert.Equal(t, "10000.admpub.com/host2", e.TypeHost(`user`, 10000, `admpub`).URI(`host2`))
+	assert.Equal(t, "10001.admpub.com/host2", e.TypeHost(`user`, H{`uid`: 10001, `name`: `admpub`}).URI(`host2`))
+
+	c, b = request(GET, "/host2", e, func(req *http.Request) {
+		req.Host = "0.coscms.com"
+	})
+	assert.Equal(t, http.StatusInternalServerError, c)
+	assert.Equal(t, http.StatusText(http.StatusInternalServerError), b)
+
+	c, b = request(GET, "/host2", e, func(req *http.Request) {
+		req.Host = "3.coscms.com"
+	})
+	assert.Equal(t, http.StatusNotFound, c)
+	assert.Equal(t, http.StatusText(http.StatusNotFound), b)
 }
