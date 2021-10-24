306abf344d7ddf31916d530c380f584659b2c3e0
Hank Shen <swhbox@foxmail.com>
2021-10-24 16:25:46 +0800

    update

--- router_test.go
+++ router_test.go
@@ -10,6 +10,9 @@ import (
 var h = HandlerFunc(func(c Context) error {
 	return c.String(`OK`)
 })
+var h2 = HandlerFunc(func(c Context) error {
+	return c.String(`OK2`)
+})
 
 func TestRouterRegexpKind(t *testing.T) {
 	e := New()
@@ -22,24 +25,57 @@ func TestRouterRegexpKind(t *testing.T) {
 	r.Add(rt, 0)
 	assert.Equal(t, `/1_%v_2/123`, rt.Format)
 	assert.Equal(t, []string{`id`}, rt.Params)
-	var h2 = HandlerFunc(func(c Context) error {
-		return c.String(`OK2`)
-	})
-	rt = &Route{
+	//fmt.Println(r.tree.String())
+	ctx := e.NewContext(nil, nil)
+	found := r.Find(`GET`, `/1_2000_2/123`, ctx)
+	assert.True(t, found)
+	assert.Equal(t, fmt.Sprintf(`%p`, h), fmt.Sprintf(`%p`, ctx.(*xContext).handler))
+}
+
+func TestRouterRegexpKind2(t *testing.T) {
+	e := New()
+	r := NewRouter(e)
+	rt := &Route{
 		Method:  `GET`,
 		Path:    `/1_<id:[\d]+>_2/123/<id2:[\d]+>`,
 		Handler: h2,
 	}
 	r.Add(rt, 0)
 	assert.Equal(t, `/1_%v_2/123/%v`, rt.Format)
-	rt = &Route{
+	//fmt.Println(r.tree.String())
+	ctx := e.NewContext(nil, nil)
+	found := r.Find(`GET`, `/1_2000_2/123/100`, ctx)
+	assert.True(t, found)
+	fmt.Println(HandlerName(ctx.(*xContext).handler))
+	assert.Equal(t, fmt.Sprintf(`%p`, h2), fmt.Sprintf(`%p`, ctx.(*xContext).handler))
+}
+
+func TestRouterRegexpKind3(t *testing.T) {
+	e := New()
+	r := NewRouter(e)
+	rt := &Route{
 		Method:  `GET`,
 		Path:    `/g/<id:[\d]+>`,
 		Handler: h2,
 	}
 	r.Add(rt, 0)
 	assert.Equal(t, `/g/%v`, rt.Format)
-	rt = &Route{
+	//fmt.Println(r.tree.String())
+
+	ctx := e.NewContext(nil, nil)
+	found := r.Find(`GET`, `/g/100`, ctx)
+	assert.True(t, found)
+	assert.Equal(t, fmt.Sprintf(`%p`, h2), fmt.Sprintf(`%p`, ctx.(*xContext).handler))
+
+	ctx = e.NewContext(nil, nil)
+	found = r.Find(`GET`, `/g/a`, ctx)
+	assert.False(t, found)
+}
+
+func TestRouterRegexpKind4(t *testing.T) {
+	e := New()
+	r := NewRouter(e)
+	rt := &Route{
 		Method:  `GET`,
 		Path:    `/g/b<id:[\d]+>`,
 		Handler: h2,
@@ -48,26 +84,7 @@ func TestRouterRegexpKind(t *testing.T) {
 	assert.Equal(t, `/g/b%v`, rt.Format)
 	//fmt.Println(r.tree.String())
 	ctx := e.NewContext(nil, nil)
-	found := r.Find(`GET`, `/1_2000_2/123`, ctx)
-	assert.True(t, found)
-	assert.Equal(t, fmt.Sprintf(`%p`, h), fmt.Sprintf(`%p`, ctx.(*xContext).handler))
-
-	ctx = e.NewContext(nil, nil)
-	found = r.Find(`GET`, `/1_2000_2/123/100`, ctx)
-	assert.True(t, found)
-	assert.Equal(t, fmt.Sprintf(`%p`, h2), fmt.Sprintf(`%p`, ctx.(*xContext).handler))
-
-	ctx = e.NewContext(nil, nil)
-	found = r.Find(`GET`, `/g/100`, ctx)
-	assert.True(t, found)
-	assert.Equal(t, fmt.Sprintf(`%p`, h2), fmt.Sprintf(`%p`, ctx.(*xContext).handler))
-
-	ctx = e.NewContext(nil, nil)
-	found = r.Find(`GET`, `/g/a`, ctx)
-	assert.False(t, found)
-
-	ctx = e.NewContext(nil, nil)
-	found = r.Find(`GET`, `/g/b100`, ctx)
+	found := r.Find(`GET`, `/g/b100`, ctx)
 	assert.True(t, found)
 	assert.Equal(t, fmt.Sprintf(`%p`, h2), fmt.Sprintf(`%p`, ctx.(*xContext).handler))
 }
@@ -80,14 +97,14 @@ func TestRouterParamKind(t *testing.T) {
 		Path:    `/:id`,
 		Handler: h,
 	}, 0)
-	fmt.Println(r.tree.String())
+	//fmt.Println(r.tree.String())
 	assert.Equal(t, `/`, string(r.tree.label))
-	assert.Equal(t, skind, r.tree.kind)
+	assert.Equal(t, staticKind, r.tree.kind)
 	assert.Equal(t, ``, r.tree.ppath)
 	assert.Equal(t, `/`, r.tree.prefix)
 
-	assert.Equal(t, `:`, string(r.tree.children[0].label))
-	assert.Equal(t, pkind, r.tree.children[0].kind)
-	assert.Equal(t, `/:id`, r.tree.children[0].ppath)
-	assert.Equal(t, `:`, r.tree.children[0].prefix)
+	assert.Equal(t, `:`, string(r.tree.paramChild.label))
+	assert.Equal(t, paramKind, r.tree.paramChild.kind)
+	assert.Equal(t, `/:id`, r.tree.paramChild.ppath)
+	assert.Equal(t, `:`, r.tree.paramChild.prefix)
 }
