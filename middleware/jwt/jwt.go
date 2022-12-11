62c22a84087648088eb8b77e009f3e8ae3ae75c1
Hank Shen <swh@admpub.com>
2022-12-11 11:41:48 +0800

    upgrade:jwt

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -1,19 +1,17 @@
 /*
+Copyright 2016 Wenhui Shen <www.webx.top>
 
-   Copyright 2016 Wenhui Shen <www.webx.top>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
 
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package middleware
 
@@ -23,7 +21,7 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/webx-top/echo"
 )
 
@@ -275,18 +273,18 @@ func jwtFromCookie(name string) jwtExtractor {
 	}
 }
 
-//BuildSignedString example: github.com/golang-jwt/jwt/example_test.go
+// BuildSignedString example: github.com/golang-jwt/jwt/example_test.go
 func BuildSignedString(claims jwt.Claims, mySigningKey interface{}) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(mySigningKey)
 }
 
-//BuildStandardSignedString example: github.com/golang-jwt/jwt/example_test.go
+// BuildStandardSignedString example: github.com/golang-jwt/jwt/example_test.go
 func BuildStandardSignedString(claims *jwt.StandardClaims, mySigningKey interface{}) (string, error) {
 	return BuildSignedString(claims, mySigningKey)
 }
 
-//BuildMapSignedString example: github.com/golang-jwt/jwt/example_test.go
+// BuildMapSignedString example: github.com/golang-jwt/jwt/example_test.go
 func BuildMapSignedString(claims jwt.MapClaims, mySigningKey interface{}) (string, error) {
 	return BuildSignedString(claims, mySigningKey)
 }
