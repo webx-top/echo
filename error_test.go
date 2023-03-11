38e713753c4a0f5f95f3ce47a02f23d8a09a3ff9
Hank Shen <swh@admpub.com>
2023-03-11 12:59:57 +0800

    update

---
+++ error_test.go
@@ -0,0 +1,17 @@
+package echo
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/webx-top/echo/code"
+)
+
+func TestInErrorCode(t *testing.T) {
+	err := NewError(`test`, code.Unauthenticated)
+	assert.True(t, IsErrorCode(err, code.Unauthenticated))
+
+	err2 := fmt.Errorf(`err2: %w`, err)
+	assert.True(t, IsErrorCode(err2, code.Unauthenticated))
+}
