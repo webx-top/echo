b9dd28e1ba24cf4a323ae3d8d90c325896437586
Hank Shen <swh@admpub.com>
2024-06-01 18:58:20 +0800

    update

--- data_kv_go1.18.go
+++ data_kv_go1.18.go
@@ -1,42 +1,44 @@
+//go:build go1.18
+
 package echo
 
 import "context"
 
-func NewKV(k, v string) *KV {
-	return &KV{K: k, V: v}
+func NewKVx[X any, Y any](k, v string) *KVx[X, Y] {
+	return &KVx[X, Y]{K: k, V: v}
 }
 
 // KV 键值对
-type KV struct {
+type KVx[X any, Y any] struct {
 	K  string
 	V  string
-	H  H           `json:",omitempty" xml:",omitempty"`
-	X  interface{} `json:",omitempty" xml:",omitempty"`
-	fn func(context.Context) interface{}
+	H  H `json:",omitempty" xml:",omitempty"`
+	X  X `json:",omitempty" xml:",omitempty"`
+	fn func(context.Context) Y
 }
 
-func (a *KV) SetK(k string) *KV {
+func (a *KVx[X, Y]) SetK(k string) *KVx[X, Y] {
 	a.K = k
 	return a
 }
 
-func (a *KV) SetV(v string) *KV {
+func (a *KVx[X, Y]) SetV(v string) *KVx[X, Y] {
 	a.V = v
 	return a
 }
 
-func (a *KV) SetKV(k, v string) *KV {
+func (a *KVx[X, Y]) SetKV(k, v string) *KVx[X, Y] {
 	a.K = k
 	a.V = v
 	return a
 }
 
-func (a *KV) SetH(h H) *KV {
+func (a *KVx[X, Y]) SetH(h H) *KVx[X, Y] {
 	a.H = h
 	return a
 }
 
-func (a *KV) SetHKV(k string, v interface{}) *KV {
+func (a *KVx[X, Y]) SetHKV(k string, v interface{}) *KVx[X, Y] {
 	if a.H == nil {
 		a.H = H{}
 	}
@@ -44,35 +46,35 @@ func (a *KV) SetHKV(k string, v interface{}) *KV {
 	return a
 }
 
-func (a *KV) SetX(x interface{}) *KV {
+func (a *KVx[X, Y]) SetX(x X) *KVx[X, Y] {
 	a.X = x
 	return a
 }
 
-func (a *KV) SetFn(fn func(context.Context) interface{}) *KV {
+func (a *KVx[X, Y]) SetFn(fn func(context.Context) Y) *KVx[X, Y] {
 	a.fn = fn
 	return a
 }
 
-func (a *KV) Fn() func(context.Context) interface{} {
+func (a *KVx[X, Y]) Fn() func(context.Context) Y {
 	return a.fn
 }
 
-type KVList []*KV
+type KVxList[X any, Y any] []*KVx[X, Y]
 
-func (list *KVList) Add(k, v string, options ...KVOption) {
-	a := &KV{K: k, V: v}
+func (list *KVxList[X, Y]) Add(k, v string, options ...KVxOption[X, Y]) {
+	a := &KVx[X, Y]{K: k, V: v}
 	for _, option := range options {
 		option(a)
 	}
 	*list = append(*list, a)
 }
 
-func (list *KVList) AddItem(item *KV) {
+func (list *KVxList[X, Y]) AddItem(item *KVx[X, Y]) {
 	*list = append(*list, item)
 }
 
-func (list *KVList) Delete(i int) {
+func (list *KVxList[X, Y]) Delete(i int) {
 	n := len(*list)
 	if i+1 < n {
 		*list = append((*list)[0:i], (*list)[i+1:]...)
@@ -81,31 +83,31 @@ func (list *KVList) Delete(i int) {
 	}
 }
 
-func (list *KVList) Reset() {
+func (list *KVxList[X, Y]) Reset() {
 	*list = (*list)[0:0]
 }
 
-// NewKVData 键值对数据
-func NewKVData() *KVData {
-	return &KVData{
-		slice: []*KV{},
+// NewKVxData 键值对数据
+func NewKVxData[X any, Y any]() *KVxData[X, Y] {
+	return &KVxData[X, Y]{
+		slice: []*KVx[X, Y]{},
 		index: map[string][]int{},
 	}
 }
 
-// KVData 键值对数据（保持顺序）
-type KVData struct {
-	slice []*KV
+// KVxData 键值对数据（保持顺序）
+type KVxData[X any, Y any] struct {
+	slice []*KVx[X, Y]
 	index map[string][]int
 }
 
 // Slice 返回切片
-func (a *KVData) Slice() []*KV {
+func (a *KVxData[X, Y]) Slice() []*KVx[X, Y] {
 	return a.slice
 }
 
 // Keys 返回所有K值
-func (a *KVData) Keys() []string {
+func (a *KVxData[X, Y]) Keys() []string {
 	keys := make([]string, len(a.slice))
 	for i, v := range a.slice {
 		if v == nil {
@@ -117,30 +119,30 @@ func (a *KVData) Keys() []string {
 }
 
 // Index 返回某个key的所有索引值
-func (a *KVData) Index(k string) []int {
+func (a *KVxData[X, Y]) Index(k string) []int {
 	v := a.index[k]
 	return v
 }
 
 // Indexes 返回所有索引值
-func (a *KVData) Indexes() map[string][]int {
+func (a *KVxData[X, Y]) Indexes() map[string][]int {
 	return a.index
 }
 
 // Reset 重置
-func (a *KVData) Reset() *KVData {
+func (a *KVxData[X, Y]) Reset() *KVxData[X, Y] {
 	a.index = map[string][]int{}
-	a.slice = []*KV{}
+	a.slice = []*KVx[X, Y]{}
 	return a
 }
 
 // Add 添加键值
-func (a *KVData) Add(k, v string, options ...KVOption) *KVData {
+func (a *KVxData[X, Y]) Add(k, v string, options ...KVxOption[X, Y]) *KVxData[X, Y] {
 	if _, y := a.index[k]; !y {
 		a.index[k] = []int{}
 	}
 	a.index[k] = append(a.index[k], len(a.slice))
-	an := &KV{K: k, V: v}
+	an := &KVx[X, Y]{K: k, V: v}
 	for _, option := range options {
 		option(an)
 	}
@@ -148,7 +150,7 @@ func (a *KVData) Add(k, v string, options ...KVOption) *KVData {
 	return a
 }
 
-func (a *KVData) AddItem(item *KV) *KVData {
+func (a *KVxData[X, Y]) AddItem(item *KVx[X, Y]) *KVxData[X, Y] {
 	if _, y := a.index[item.K]; !y {
 		a.index[item.K] = []int{}
 	}
@@ -158,23 +160,23 @@ func (a *KVData) AddItem(item *KV) *KVData {
 }
 
 // Set 设置首个键值
-func (a *KVData) Set(k, v string, options ...KVOption) *KVData {
+func (a *KVxData[X, Y]) Set(k, v string, options ...KVxOption[X, Y]) *KVxData[X, Y] {
 	a.index[k] = []int{0}
-	an := &KV{K: k, V: v}
+	an := &KVx[X, Y]{K: k, V: v}
 	for _, option := range options {
 		option(an)
 	}
-	a.slice = []*KV{an}
+	a.slice = []*KVx[X, Y]{an}
 	return a
 }
 
-func (a *KVData) SetItem(item *KV) *KVData {
+func (a *KVxData[X, Y]) SetItem(item *KVx[X, Y]) *KVxData[X, Y] {
 	a.index[item.K] = []int{0}
-	a.slice = []*KV{item}
+	a.slice = []*KVx[X, Y]{item}
 	return a
 }
 
-func (a *KVData) Get(k string, defaults ...string) string {
+func (a *KVxData[X, Y]) Get(k string, defaults ...string) string {
 	if indexes, ok := a.index[k]; ok {
 		if len(indexes) > 0 {
 			return a.slice[indexes[0]].V
@@ -186,7 +188,7 @@ func (a *KVData) Get(k string, defaults ...string) string {
 	return ``
 }
 
-func (a *KVData) GetItem(k string, defaults ...func() *KV) *KV {
+func (a *KVxData[X, Y]) GetItem(k string, defaults ...func() *KVx[X, Y]) *KVx[X, Y] {
 	if indexes, ok := a.index[k]; ok {
 		if len(indexes) > 0 {
 			return a.slice[indexes[0]]
@@ -198,7 +200,7 @@ func (a *KVData) GetItem(k string, defaults ...func() *KV) *KV {
 	return nil
 }
 
-func (a *KVData) Has(k string) bool {
+func (a *KVxData[X, Y]) Has(k string) bool {
 	if _, ok := a.index[k]; ok {
 		return true
 	}
@@ -206,7 +208,7 @@ func (a *KVData) Has(k string) bool {
 }
 
 // Delete 设置某个键的所有值
-func (a *KVData) Delete(ks ...string) *KVData {
+func (a *KVxData[X, Y]) Delete(ks ...string) *KVxData[X, Y] {
 	indexes := []int{}
 	for _, k := range ks {
 		v, y := a.index[k]
@@ -215,7 +217,7 @@ func (a *KVData) Delete(ks ...string) *KVData {
 		}
 		indexes = append(indexes, v...)
 	}
-	newSlice := []*KV{}
+	newSlice := []*KVx[X, Y]{}
 	a.index = map[string][]int{}
 	for i, v := range a.slice {
 		var exists bool
---
+++ data_kv_option_go1.18 .go
@@ -0,0 +1,46 @@
+//go:build go1.18
+
+package echo
+
+import "context"
+
+type KVxOption[X any, Y any] func(*KVx[X, Y])
+
+func KVxOptK[X any, Y any](k string) KVxOption[X, Y] {
+	return func(a *KVx[X, Y]) {
+		a.K = k
+	}
+}
+
+func KVxOptV[X any, Y any](v string) KVxOption[X, Y] {
+	return func(a *KVx[X, Y]) {
+		a.V = v
+	}
+}
+
+func KVxOptH[X any, Y any](h H) KVxOption[X, Y] {
+	return func(a *KVx[X, Y]) {
+		a.H = h
+	}
+}
+
+func KVxOptHKV[X any, Y any](k string, v interface{}) KVxOption[X, Y] {
+	return func(a *KVx[X, Y]) {
+		if a.H == nil {
+			a.H = H{}
+		}
+		a.H.Set(k, v)
+	}
+}
+
+func KVxOptX[X any, Y any](x X) KVxOption[X, Y] {
+	return func(a *KVx[X, Y]) {
+		a.X = x
+	}
+}
+
+func KVxOptFn[X any, Y any](fn func(context.Context) Y) KVxOption[X, Y] {
+	return func(a *KVx[X, Y]) {
+		a.fn = fn
+	}
+}
