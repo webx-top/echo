20733b8fe03660728891bb137b1c4c45de31b0c4
Hank Shen <swh@admpub.com>
2025-03-22 16:50:16 +0800

    update

--- encoding/datatype/unix_time.go
+++ encoding/datatype/unix_time.go
@@ -11,3 +11,10 @@ func (u UnixTime) MarshalText() ([]byte, error) {
 	seconds := time.Time(u).Unix()
 	return []byte(strconv.FormatInt(seconds, 10)), nil
 }
+
+type UnixMilli time.Time
+
+func (u UnixMilli) MarshalText() ([]byte, error) {
+	seconds := time.Time(u).UnixMilli()
+	return []byte(strconv.FormatInt(seconds, 10)), nil
+}
